a5b9687fdb45ee6a22708f9cbaa30828275f8ddc
DanielZellner <daniel.zellner@edu.fh-joanneum.at>
2022-11-06 18:33:09 +0100

    init

---
+++ main.js
@@ -0,0 +1,415 @@
+let random;
+
+function randomEvent(){
+    random = Math.random() * 100;
+    console.log(Math.round(random))
+    findEvent()
+}
+function findEvent(){
+    switch (Math.round(random))
+    {
+        case 1:
+            document.getElementById("text").innerHTML = "Thief";
+            break;
+
+        case 2:
+            document.getElementById("text").innerHTML = "Thief";
+            break;
+
+        case 3:
+            document.getElementById("text").innerHTML = "Thief";
+            break;
+
+        case 4:
+            document.getElementById("text").innerHTML = "Thief";
+            break;
+
+        case 5:
+            document.getElementById("text").innerHTML = "Swap";
+            break;
+
+        case 6:
+            document.getElementById("text").innerHTML = "Swap";
+            break;
+
+        case 7:
+            document.getElementById("text").innerHTML = "Swap";
+            break;
+
+        case 8:
+            document.getElementById("text").innerHTML = "Switch";
+            break;
+
+        case 9:
+            document.getElementById("text").innerHTML = "Switch";
+            break;
+
+        case 10:
+            document.getElementById("text").innerHTML = "Lose";
+            break;
+
+        case 11:
+            document.getElementById("text").innerHTML = "Lose";
+            break;
+
+        case 12:
+            document.getElementById("text").innerHTML = "Lose";
+            break;
+
+        case 13:
+            document.getElementById("text").innerHTML = "Gain 2";
+            break;
+
+        case 14:
+            document.getElementById("text").innerHTML = "Gain 1";
+            break;
+
+        case 15:
+            document.getElementById("text").innerHTML = "Gain 3";
+            break;
+
+        case 16:
+            document.getElementById("text").innerHTML = "Gain 4";
+            break;
+
+        case 17:
+            document.getElementById("text").innerHTML = "Aufräumdienst";
+            break;
+
+        case 18:
+            document.getElementById("text").innerHTML = "Nope";
+            break;
+
+        case 19:
+            document.getElementById("text").innerHTML = "Double Trouble";
+            break;
+
+        case 20:
+            document.getElementById("text").innerHTML = "Nope";
+            break;
+
+        case 21:
+            document.getElementById("text").innerHTML = "Joker";
+            break;
+
+        case 22:
+            document.getElementById("text").innerHTML = "Joker";
+            break;
+
+        case 23:
+            document.getElementById("text").innerHTML = "Joker";
+            break;
+
+        case 24:
+            document.getElementById("text").innerHTML = "Joker";
+            break;
+
+        case 25:
+            document.getElementById("text").innerHTML = "Reverse";
+            break;
+
+        case 26:
+            document.getElementById("text").innerHTML = "Reverse";
+            break;
+
+        case 27:
+            document.getElementById("text").innerHTML = "Reverse";
+            break;
+
+        case 28:
+            document.getElementById("text").innerHTML = "Surprise";
+            break;
+
+        case 29:
+            document.getElementById("text").innerHTML = "Double";
+            break;
+
+        case 30:
+            document.getElementById("text").innerHTML = "Double";
+            break;
+
+        case 31:
+            document.getElementById("text").innerHTML = "Double";
+            break;
+
+        case 32:
+            document.getElementById("text").innerHTML = "Turn around";
+            break;
+
+        case 33:
+            document.getElementById("text").innerHTML = "Turn around";
+            break;
+
+        case 34:
+            document.getElementById("text").innerHTML = "Hell";
+            break;
+
+        case 35:
+            document.getElementById("text").innerHTML = "Hell";
+            break;
+
+        case 36:
+            document.getElementById("text").innerHTML = "Heaven";
+            break;
+
+        case 37:
+            document.getElementById("text").innerHTML = "Heaven";
+            break;
+
+        case 38:
+            document.getElementById("text").innerHTML = "Deal";
+            break;
+
+        case 39:
+            document.getElementById("text").innerHTML = "Deal";
+            break;
+
+        case 40:
+            document.getElementById("text").innerHTML = "Surprise";
+            break;
+
+        case 41:
+            document.getElementById("text").innerHTML = "Drink 2";
+            break;
+
+        case 42:
+            document.getElementById("text").innerHTML = "Drink 4";
+            break;
+
+        case 43:
+            document.getElementById("text").innerHTML = "Drink 6";
+            break;
+
+        case 44:
+            document.getElementById("text").innerHTML = "Drink 8";
+            break;
+
+        case 45:
+            document.getElementById("text").innerHTML = "Double Trouble";
+            break;
+
+        case 46:
+            document.getElementById("text").innerHTML = "+ Ticket";
+            break;
+
+        case 47:
+            document.getElementById("text").innerHTML = "- Ticket";
+            break;
+
+        case 48:
+            document.getElementById("text").innerHTML = "- Ticket";
+            break;
+
+        case 49:
+            document.getElementById("text").innerHTML = "+ Ticket";
+            break;
+
+        case 50:
+            document.getElementById("text").innerHTML = "Rule";
+            break;
+
+        case 51:
+            document.getElementById("text").innerHTML = "Ultra Thief";
+            break;
+
+        case 52:
+            document.getElementById("text").innerHTML = "Ultra Thief";
+            break;
+
+        case 53:
+            document.getElementById("text").innerHTML = "Tausch";
+            break;
+
+        case 54:
+            document.getElementById("text").innerHTML = "Tausch";
+            break;
+
+        case 55:
+            document.getElementById("text").innerHTML = "Tausch";
+            break;
+
+        case 56:
+            document.getElementById("text").innerHTML = "Nothing";
+            break;
+
+        case 57:
+            document.getElementById("text").innerHTML = "Choose";
+            break;
+
+        case 58:
+            document.getElementById("text").innerHTML = "Choose";
+            break;
+
+        case 59:
+            document.getElementById("text").innerHTML = "Training";
+            break;
+
+        case 60:
+            document.getElementById("text").innerHTML = "Training";
+            break;
+
+        case 61:
+            document.getElementById("text").innerHTML = "Truth or Dare";
+            break;
+
+        case 62:
+            document.getElementById("text").innerHTML = "Truth or Dare";
+            break;
+
+        case 63:
+            document.getElementById("text").innerHTML = "Casino";
+            break;
+
+        case 64:
+            document.getElementById("text").innerHTML = "Casino";
+            break;
+
+        case 65:
+            document.getElementById("text").innerHTML = "Casino";
+            break;
+
+        case 66:
+            document.getElementById("text").innerHTML = "Skip black";
+            break;
+
+        case 67:
+            document.getElementById("text").innerHTML = "Skip red";
+            break;
+
+        case 68:
+            document.getElementById("text").innerHTML = "Skip black";
+            break;
+
+        case 69:
+            document.getElementById("text").innerHTML = "Skip red";
+            break;
+
+        case 70:
+            document.getElementById("text").innerHTML = "Draw";
+            break;
+
+        case 71:
+            document.getElementById("text").innerHTML = "Draw";
+            break;
+
+        case 72:
+            document.getElementById("text").innerHTML = "Draw";
+            break;
+
+        case 73:
+            document.getElementById("text").innerHTML = "Eye";
+            break;
+
+        case 74:
+            document.getElementById("text").innerHTML = "Eye";
+            break;
+
+        case 75:
+            document.getElementById("text").innerHTML = "Glasses";
+            break;
+
+        case 76:
+            document.getElementById("text").innerHTML = "Glasses";
+            break;
+
+        case 77:
+            document.getElementById("text").innerHTML = "High Pot";
+            break;
+
+        case 78:
+            document.getElementById("text").innerHTML = "High Pot";
+            break;
+
+        case 79:
+            document.getElementById("text").innerHTML = "Cheap";
+            break;
+
+        case 80:
+            document.getElementById("text").innerHTML = "Cheap";
+            break;
+
+        case 81:
+            document.getElementById("text").innerHTML = "Expensive";
+            break;
+
+        case 82:
+            document.getElementById("text").innerHTML = "Expensive";
+            break;
+
+        case 83:
+            document.getElementById("text").innerHTML = "Gift";
+            break;
+
+        case 84:
+            document.getElementById("text").innerHTML = "Dice";
+            break;
+
+        case 85:
+            document.getElementById("text").innerHTML = "Dice";
+            break;
+
+        case 86:
+            document.getElementById("text").innerHTML = "Russian Roulette";
+            break;
+
+        case 87:
+            document.getElementById("text").innerHTML = "Ex";
+            break;
+
+        case 88:
+            document.getElementById("text").innerHTML = "Aufräumdienst";
+            break;
+
+        case 89:
+            document.getElementById("text").innerHTML = "Exchange";
+            break;
+
+        case 90:
+            document.getElementById("text").innerHTML = "Exchange";
+            break;
+
+        case 91:
+            document.getElementById("text").innerHTML = "Share";
+            break;
+
+        case 92:
+            document.getElementById("text").innerHTML = "Share";
+            break;
+
+        case 93:
+            document.getElementById("text").innerHTML = "Water";
+            break;
+
+        case 94:
+            document.getElementById("text").innerHTML = "Dealership";
+            break;
+
+        case 95:
+            document.getElementById("text").innerHTML = "Dealership";
+            break;
+
+        case 96:
+            document.getElementById("text").innerHTML = "Bitch";
+            break;
+
+        case 97:
+            document.getElementById("text").innerHTML = "Bitch";
+            break;
+
+        case 98:
+            document.getElementById("text").innerHTML = "Bad luck";
+            break;
+
+        case 99:
+            document.getElementById("text").innerHTML = "Bad luck";
+            break;
+
+        case 100:
+            document.getElementById("text").innerHTML = "Good luck";
+            break;
+
+        default:
+            document.getElementById("text").innerHTML = "Drink 1";
+            break;
+    }
+}
