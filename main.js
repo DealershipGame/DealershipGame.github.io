406e5716d8baa460530aeba814822d9e05b1a6bc
DanielZellner <daniel.zellner@edu.fh-joanneum.at>
2022-11-07 13:03:53 +0100

    feat

--- main.js
+++ main.js
@@ -10,406 +10,507 @@ function findEvent(){
     {
         case 1:
             document.getElementById("text").innerHTML = "Thief";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 2:
             document.getElementById("text").innerHTML = "Thief";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 3:
             document.getElementById("text").innerHTML = "Thief";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 4:
             document.getElementById("text").innerHTML = "Thief";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 5:
             document.getElementById("text").innerHTML = "Swap";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 6:
             document.getElementById("text").innerHTML = "Swap";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 7:
             document.getElementById("text").innerHTML = "Swap";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 8:
             document.getElementById("text").innerHTML = "Switch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 9:
             document.getElementById("text").innerHTML = "Switch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 10:
             document.getElementById("text").innerHTML = "Lose";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 11:
             document.getElementById("text").innerHTML = "Lose";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 12:
             document.getElementById("text").innerHTML = "Lose";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 13:
             document.getElementById("text").innerHTML = "Gain 2";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 14:
             document.getElementById("text").innerHTML = "Gain 1";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 15:
             document.getElementById("text").innerHTML = "Gain 3";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 16:
             document.getElementById("text").innerHTML = "Gain 4";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 17:
             document.getElementById("text").innerHTML = "Aufräumdienst";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 18:
             document.getElementById("text").innerHTML = "Nope";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 19:
             document.getElementById("text").innerHTML = "Double Trouble";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 20:
             document.getElementById("text").innerHTML = "Nope";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 21:
             document.getElementById("text").innerHTML = "Joker";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 22:
             document.getElementById("text").innerHTML = "Joker";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 23:
             document.getElementById("text").innerHTML = "Joker";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 24:
             document.getElementById("text").innerHTML = "Joker";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 25:
             document.getElementById("text").innerHTML = "Reverse";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 26:
             document.getElementById("text").innerHTML = "Reverse";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 27:
             document.getElementById("text").innerHTML = "Reverse";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 28:
             document.getElementById("text").innerHTML = "Surprise";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 29:
             document.getElementById("text").innerHTML = "Double";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 30:
             document.getElementById("text").innerHTML = "Double";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 31:
             document.getElementById("text").innerHTML = "Double";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 32:
             document.getElementById("text").innerHTML = "Turn around";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 33:
             document.getElementById("text").innerHTML = "Turn around";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 34:
             document.getElementById("text").innerHTML = "Hell";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 35:
             document.getElementById("text").innerHTML = "Hell";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 36:
             document.getElementById("text").innerHTML = "Heaven";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 37:
             document.getElementById("text").innerHTML = "Heaven";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 38:
             document.getElementById("text").innerHTML = "Deal";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 39:
             document.getElementById("text").innerHTML = "Deal";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 40:
             document.getElementById("text").innerHTML = "Surprise";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 41:
             document.getElementById("text").innerHTML = "Drink 2";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 42:
             document.getElementById("text").innerHTML = "Drink 4";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 43:
             document.getElementById("text").innerHTML = "Drink 6";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 44:
             document.getElementById("text").innerHTML = "Drink 8";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 45:
             document.getElementById("text").innerHTML = "Double Trouble";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 46:
             document.getElementById("text").innerHTML = "+ Ticket";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 47:
             document.getElementById("text").innerHTML = "- Ticket";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 48:
             document.getElementById("text").innerHTML = "- Ticket";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 49:
             document.getElementById("text").innerHTML = "+ Ticket";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 50:
             document.getElementById("text").innerHTML = "Rule";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 51:
             document.getElementById("text").innerHTML = "Ultra Thief";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 52:
             document.getElementById("text").innerHTML = "Ultra Thief";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 53:
             document.getElementById("text").innerHTML = "Tausch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 54:
             document.getElementById("text").innerHTML = "Tausch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 55:
             document.getElementById("text").innerHTML = "Tausch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 56:
             document.getElementById("text").innerHTML = "Nothing";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 57:
             document.getElementById("text").innerHTML = "Choose";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 58:
             document.getElementById("text").innerHTML = "Choose";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 59:
             document.getElementById("text").innerHTML = "Training";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 60:
             document.getElementById("text").innerHTML = "Training";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 61:
             document.getElementById("text").innerHTML = "Truth or Dare";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 62:
             document.getElementById("text").innerHTML = "Truth or Dare";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 63:
             document.getElementById("text").innerHTML = "Casino";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 64:
             document.getElementById("text").innerHTML = "Casino";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 65:
             document.getElementById("text").innerHTML = "Casino";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 66:
             document.getElementById("text").innerHTML = "Skip black";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 67:
             document.getElementById("text").innerHTML = "Skip red";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 68:
             document.getElementById("text").innerHTML = "Skip black";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 69:
             document.getElementById("text").innerHTML = "Skip red";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 70:
             document.getElementById("text").innerHTML = "Draw";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 71:
             document.getElementById("text").innerHTML = "Draw";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 72:
             document.getElementById("text").innerHTML = "Draw";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 73:
             document.getElementById("text").innerHTML = "Eye";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 74:
             document.getElementById("text").innerHTML = "Eye";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 75:
             document.getElementById("text").innerHTML = "Glasses";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 76:
             document.getElementById("text").innerHTML = "Glasses";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 77:
             document.getElementById("text").innerHTML = "High Pot";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 78:
             document.getElementById("text").innerHTML = "High Pot";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 79:
             document.getElementById("text").innerHTML = "Cheap";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 80:
             document.getElementById("text").innerHTML = "Cheap";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 81:
             document.getElementById("text").innerHTML = "Expensive";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 82:
             document.getElementById("text").innerHTML = "Expensive";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 83:
             document.getElementById("text").innerHTML = "Gift";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 84:
             document.getElementById("text").innerHTML = "Dice";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 85:
             document.getElementById("text").innerHTML = "Dice";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 86:
             document.getElementById("text").innerHTML = "Russian Roulette";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 87:
             document.getElementById("text").innerHTML = "Ex";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 88:
             document.getElementById("text").innerHTML = "Aufräumdienst";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 89:
             document.getElementById("text").innerHTML = "Exchange";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 90:
             document.getElementById("text").innerHTML = "Exchange";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 91:
             document.getElementById("text").innerHTML = "Share";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 92:
             document.getElementById("text").innerHTML = "Share";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 93:
             document.getElementById("text").innerHTML = "Water";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 94:
             document.getElementById("text").innerHTML = "Dealership";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 95:
             document.getElementById("text").innerHTML = "Dealership";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 96:
             document.getElementById("text").innerHTML = "Bitch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 97:
             document.getElementById("text").innerHTML = "Bitch";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 98:
             document.getElementById("text").innerHTML = "Bad luck";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 99:
             document.getElementById("text").innerHTML = "Bad luck";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         case 100:
             document.getElementById("text").innerHTML = "Good luck";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
 
         default:
             document.getElementById("text").innerHTML = "Drink 1";
+            document.getElementById("explain").innerHTML = "not added yet";
             break;
     }
 }
